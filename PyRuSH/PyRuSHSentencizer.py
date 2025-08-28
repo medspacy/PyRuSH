538ca5917f3f4bcbc65beb23e4c7b5de164d7bca
jianlins <jianlinshi.cn@gmail.com>
2025-08-27 21:10:46 -0600

    Add max_sentence_length parameter to PyRuSHSentencizer and update related methods

--- PyRuSH/PyRuSHSentencizer.py
+++ PyRuSH/PyRuSHSentencizer.py
@@ -25,16 +25,22 @@ from .StaticSentencizerFun import cpredict_merge_gaps,cpredict_split_gaps, cset_
 @Language.factory("medspacy_pyrush")
 class PyRuSHSentencizer(Sentencizer):
     def __init__(self, nlp: Language, name: str = "medspacy_pyrush", rules_path: str = '', max_repeat: int = 50,
-                 auto_fix_gaps: bool = True, merge_gaps: bool = False) -> Sentencizer:
+                 auto_fix_gaps: bool = True, merge_gaps: bool = False, max_sentence_length: int = None) -> Sentencizer:
         """
+        Initialize the PyRuSH sentencizer component.
 
-        @param rules_path: The string of the rule file path or rules themselves. By default, it will look for
-        rush_rules.tsv in the site_packages/conf folder.
-        @param max_repeat: Total number of replicates that allows to be handled by "+" wildcard.
-        @param auto_fix_gaps: If gaps are caused by malcrafted rules, try to fix them.
-            However, this has no control of sentence end,
-        @param merge_gaps: When True, gaps between sentences are merged into the preceding sentence.
-            When False, gaps are split into separate sentences.
+        Args:
+            nlp (Language): The spaCy language pipeline.
+            name (str): Name of the component. Default is "medspacy_pyrush".
+            rules_path (str): Path to the rule file or rules themselves. If empty, defaults to 'conf/rush_rules.tsv'.
+            max_repeat (int): Maximum number of repeats allowed for the '+' wildcard in rules.
+            auto_fix_gaps (bool): If True, attempts to fix gaps caused by malformed rules.
+            merge_gaps (bool): If True, merges gaps between sentences into the preceding sentence. If False, splits gaps into separate sentences.
+            max_sentence_length (int or None): Maximum allowed sentence length in characters. If set, sentences longer than this will be split.
+
+        Notes:
+            - Setting merge_gaps controls whether gaps are merged or split.
+            - max_sentence_length applies to both merge and split modes.
         """
         self.nlp = nlp
         self.name = name
@@ -45,32 +51,66 @@ class PyRuSHSentencizer(Sentencizer):
         self.rules_path = rules_path
         self.rush = RuSH(rules=rules_path, max_repeat=max_repeat, auto_fix_gaps=auto_fix_gaps)
         self.merge_gaps = merge_gaps
+        self.max_sentence_length = max_sentence_length
 
     @classmethod
     def from_nlp(cls, nlp, **cfg):
+        """
+        Create a PyRuSHSentencizer instance from a spaCy nlp object and configuration.
+
+        Args:
+            nlp (Language): The spaCy language pipeline.
+            **cfg: Additional configuration parameters for initialization.
+
+        Returns:
+            PyRuSHSentencizer: An initialized sentencizer instance.
+        """
         return cls(**cfg)
 
     def __call__(self, doc):
+        """
+        Apply sentence boundary detection to a spaCy Doc and set sentence start annotations.
+
+        Args:
+            doc (Doc): The spaCy Doc to process.
+
+        Returns:
+            Doc: The processed Doc with sentence boundaries set.
+        """
         tags = self.predict([doc])
         cset_annotations([doc], tags)
         return doc
 
     def predict(self, docs):
-        """Apply the pipeline's model to a batch of docs, without
-        modifying them.
+        """
+        Predict sentence boundaries for a batch of spaCy Docs.
+
+        Args:
+            docs (list of Doc): List of spaCy Docs to process.
+
+        Returns:
+            list of list of bool: Sentence start guesses for each Doc.
+
+        Notes:
+            - Does not modify the Docs; only returns sentence start predictions.
         """
         if self.merge_gaps:
             from .StaticSentencizerFun import cpredict_ww
-            guesses = cpredict_merge_gaps(docs, self.rush.segToSentenceSpans)
+            guesses = cpredict_merge_gaps(docs, self.rush.segToSentenceSpans, self.max_sentence_length)
         else:
-            guesses = cpredict_split_gaps(docs, self.rush.segToSentenceSpans)
+            guesses = cpredict_split_gaps(docs, self.rush.segToSentenceSpans, self.max_sentence_length)
         return guesses
 
     def set_annotations(self, docs, batch_tag_ids, tensors=None):
         """
-        This function overwrite spacy's Sentencizer.
+        Set sentence boundary annotations on spaCy Docs.
 
-        @param batch_tag_ids: a list of doc's tags (a list of boolean values)
-        @param tensors: a place holder for future extensions
+        Args:
+            docs (list of Doc): List of spaCy Docs to annotate.
+            batch_tag_ids (list of list of bool): Sentence start tags for each Doc.
+            tensors: Placeholder for future extensions (optional).
+
+        Notes:
+            - This method overwrites spaCy's Sentencizer annotations.
         """
         cset_annotations(docs, batch_tag_ids, tensors)
